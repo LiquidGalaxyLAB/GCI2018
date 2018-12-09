5c6530542829efa6aea221a82f92b367561a817c
arnav aggarwal <aggarnav@gmail.com>
2018-12-09 21:08:07 +0530

    updated osc 2

--- jon_snow/SUPER TOP TEN TASK, a winner one Program a simple OSC protocol based Liquid Galaxy Android application/Android/app/src/main/java/com/dhua/osctutorial/MainActivity.java
+++ jon_snow/SUPER TOP TEN TASK, a winner one Program a simple OSC protocol based Liquid Galaxy Android application/Android/app/src/main/java/com/dhua/osctutorial/MainActivity.java
@@ -10,8 +10,8 @@ import com.dhua.osctutorial.Osc;
 
 public class MainActivity extends Activity {
 
-    EditText ip;
     Osc osc;
+    final String ip = "192.168.1.4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,53 +20,44 @@ public class MainActivity extends Activity {
 
     }
     public void showEarth(View view){
-        ip   = (EditText)findViewById(R.id.ip);
-        osc.oscRequest(ip.getText().toString(), "earth");
+        osc.oscRequest(ip, "earth");
 
     }
     public void showMars(View view){
-        ip   = (EditText)findViewById(R.id.ip);
-        osc.oscRequest(ip.getText().toString(), "mars");
+        osc.oscRequest(ip, "mars");
 
     }
     public void showMoon(View view){
-        ip   = (EditText)findViewById(R.id.ip);
-        osc.oscRequest(ip.getText().toString(), "moon");
+        osc.oscRequest(ip, "moon");
 
     }
     public void showSky(View view){
-        ip   = (EditText)findViewById(R.id.ip);
-        osc.oscRequest(ip.getText().toString(), "sky");
+        osc.oscRequest(ip, "sky");
 
     }
 
     public void delhi(View view) {
-        ip   = (EditText)findViewById(R.id.ip);
         final String latlong   ="New Delhi,77.1025,28.7041";
-        osc.oscRequest(ip.getText().toString(), latlong);
+        osc.oscRequest(ip, latlong);
     }
 
     public void newYork(View view) {
-        ip   = (EditText)findViewById(R.id.ip);
         final String latlong   ="New York City,-74.006028,40.7128";
-        osc.oscRequest(ip.getText().toString(), latlong);
+        osc.oscRequest(ip, latlong);
     }
 
     public void sanJose(View view) {
-        ip   = (EditText)findViewById(R.id.ip);
         final String latlong   ="San Jose,-121.886328,37.3382";
-        osc.oscRequest(ip.getText().toString(), latlong);
+        osc.oscRequest(ip, latlong);
     }
 
     public void shanghai(View view) {
-        ip   = (EditText)findViewById(R.id.ip);
         final String latlong   ="Shanghai,121.4737,31.2304";
-        osc.oscRequest(ip.getText().toString(), latlong);
+        osc.oscRequest(ip, latlong);
     }
 
     public void london(View view) {
-        ip   = (EditText)findViewById(R.id.ip);
         final String latlong   ="London,0.127,51.5074";
-        osc.oscRequest(ip.getText().toString(), latlong);
+        osc.oscRequest(ip, latlong);
     }
 }
